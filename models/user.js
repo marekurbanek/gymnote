95a43817f5c9bfccc651cdc6653e1b5423d4c7f8
Urbanek <marek.urbanek@accenture.com>
2019-02-06 15:57:16 +0100

    Added user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  login: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+}, {
+  timestamps: false
+})
+
+module.exports = User
