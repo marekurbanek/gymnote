16dfd82219d16653e5c1fae9dc8fa0981e871e09
Urbanek <marek.urbanek@accenture.com>
2019-02-07 10:44:15 +0100

    Using env variable for db connection

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('demo_schema', 'root', 'admin', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorsAliases: false
-})
+if (process.env.CLEARDB_DATABASE_URL) {
+  module.exports = new Sequelize(process.env.CLEARDB_DATABASE_URL)
+} else {
+  module.exports = new Sequelize('demo_schema', 'root', 'admin', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false
+  })
+}
